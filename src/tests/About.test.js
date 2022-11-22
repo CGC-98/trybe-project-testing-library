63db3cd7e88513cbef27cb7ae290959d84556dad
CGC-98 <caiquecorreia.gama@hotmail.com>
2022-11-22 16:45:29 -0300

    q2; <About.js />

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,29 @@
-test('', () => {});
+// Teste se a página contém as informações sobre a Pokédex;
+
+import { screen } from '@testing-library/react';
+// import userEvent from '@testing-library/user-event';
+import renderWithRouter from '../renderWithRouter';
+import About from '../pages/About';
+
+test('Find Pokédex info', () => {
+  renderWithRouter(<About />);
+
+  const aboutHeader = screen.getByRole('heading', { name: 'About Pokédex', level: 2 });
+  const aboutP1 = screen.getByText(/This application simulates a Pokédex/);
+  const aboutP2 = screen.getByText(/One can filter Pokémon by type/);
+
+  expect(aboutHeader).toBeInTheDocument();
+  expect(aboutP1).toBeInTheDocument();
+  expect(aboutP2).toBeInTheDocument();
+});
+
+test('Find Pokédex images', () => {
+  renderWithRouter(<About />);
+
+  const pokedexImg = screen.getByRole('img');
+  const pokedexImgURL = 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png';
+
+  expect(pokedexImg).toBeInTheDocument();
+  expect(pokedexImg).toHaveAttribute('src', pokedexImgURL);
+  expect(pokedexImg).toHaveAttribute('alt', 'Pokédex');
+});
