8259880605114dbb6c995095899479b9ecafbb93
CGC-98 <caiquecorreia.gama@hotmail.com>
2022-11-22 16:16:10 -0300

    <App.js /> description remove

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -3,57 +3,54 @@ import userEvent from '@testing-library/user-event';
 import renderWithRouter from '../renderWithRouter';
 import App from '../App';
 
-describe('', () => {
+test('Verify existing links', () => {
   renderWithRouter(<App />);
+  const homeLink = screen.getByRole('link', { name: 'Home' });
+  const aboutLink = screen.getByRole('link', { name: 'About' });
+  const favPokemonLink = screen.getByRole('link', { name: 'Favorite Pokémon' });
 
-  test('Verify existing links', () => {
-    const homeLink = screen.getByRole('link', { name: 'Home' });
-    const aboutLink = screen.getByRole('link', { name: 'About' });
-    const favPokemonLink = screen.getByRole('link', { name: 'Favorite Pokémon' });
+  expect(homeLink).toBeInTheDocument();
+  expect(aboutLink).toBeInTheDocument();
+  expect(favPokemonLink).toBeInTheDocument();
+});
 
-    expect(homeLink).toBeInTheDocument();
-    expect(aboutLink).toBeInTheDocument();
-    expect(favPokemonLink).toBeInTheDocument();
+test('Verify "/" redirect', () => {
+  const { history } = renderWithRouter(<App />);
+
+  const homeLink = screen.getByRole('link', { name: 'Home' });
+  userEvent.click(homeLink);
+
+  const { pathname } = history.location;
+  expect(pathname).toBe('/');
+});
+
+test('Verify "/about" redirect', async () => {
+  const { history } = renderWithRouter(<App />);
+
+  const aboutLink = screen.getByRole('link', { name: 'About' });
+  userEvent.click(aboutLink);
+
+  const { pathname } = history.location;
+  expect(pathname).toBe('/about');
+});
+
+test('Verify "/favorites" redirect', () => {
+  const { history } = renderWithRouter(<App />);
+
+  const favPokemonLink = screen.getByRole('link', { name: 'Favorite Pokémon' });
+  userEvent.click(favPokemonLink);
+
+  const { pathname } = history.location;
+  expect(pathname).toBe('/favorites');
+});
+
+test('Verify redirect to unexistent route', async () => {
+  const { history } = renderWithRouter(<App />);
+
+  act(() => {
+    history.push('/non/existent/page');
   });
 
-  test('Verify "/" redirect', () => {
-    const { history } = renderWithRouter(<App />);
-
-    const homeLink = screen.getByRole('link', { name: 'Home' });
-    userEvent.click(homeLink);
-
-    const { pathname } = history.location;
-    expect(pathname).toBe('/');
-  });
-
-  test('Verify "/about" redirect', async () => {
-    const { history } = renderWithRouter(<App />);
-
-    const aboutLink = screen.getByRole('link', { name: 'About' });
-    userEvent.click(aboutLink);
-
-    const { pathname } = history.location;
-    expect(pathname).toBe('/about');
-  });
-
-  test('Verify "/favorites" redirect', () => {
-    const { history } = renderWithRouter(<App />);
-
-    const favPokemonLink = screen.getByRole('link', { name: 'Favorite Pokémon' });
-    userEvent.click(favPokemonLink);
-
-    const { pathname } = history.location;
-    expect(pathname).toBe('/favorites');
-  });
-
-  test('Verify redirect to unexistent route', async () => {
-    const { history } = renderWithRouter(<App />);
-
-    act(() => {
-      history.push('/non/existent/page');
-    });
-
-    const notFound = await screen.findByRole('heading', { name: 'Page requested not found', level: 2 });
-    expect(notFound).toBeInTheDocument();
-  });
+  const notFound = await screen.findByRole('heading', { name: 'Page requested not found', level: 2 });
+  expect(notFound).toBeInTheDocument();
 });
