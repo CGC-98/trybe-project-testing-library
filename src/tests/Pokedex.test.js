42453c066be4b1de80f135c3c226ecdbac3be079
CGC-98 <caiquecorreia.gama@hotmail.com>
2022-11-24 11:38:43 -0300

    q5; code cleaning

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -10,60 +10,47 @@ test('Verify "Encountered Pokémon" h2', () => {
   expect(encounterHeader).toBeInTheDocument();
 });
 
-test('Verify existence of all buttons', () => {
+test('Verify existence and functionality of all buttons', () => {
   renderWithRouter(<App />);
 
   const allButton = screen.getByRole('button', { name: 'All' });
-  const electricButton = screen.getByRole('button', { name: 'Electric' });
-  const fireButton = screen.getByRole('button', { name: 'Fire' });
-  const bugButton = screen.getByRole('button', { name: 'Bug' });
-  const poisonButton = screen.getByRole('button', { name: 'Poison' });
-  const psychicButton = screen.getByRole('button', { name: 'Psychic' });
-  const normalButton = screen.getByRole('button', { name: 'Normal' });
-  const dragonButton = screen.getByRole('button', { name: 'Dragon' });
   const nextButton = screen.getByRole('button', { name: 'Próximo Pokémon' });
 
   expect(allButton).toBeInTheDocument();
-  expect(electricButton).toBeInTheDocument();
-  expect(fireButton).toBeInTheDocument();
-  expect(bugButton).toBeInTheDocument();
-  expect(poisonButton).toBeInTheDocument();
-  expect(psychicButton).toBeInTheDocument();
-  expect(normalButton).toBeInTheDocument();
-  expect(dragonButton).toBeInTheDocument();
   expect(nextButton).toBeInTheDocument();
+
+  const listOfTypes = ['Electric', 'Fire', 'Bug', 'Poison', 'Psychic', 'Normal', 'Dragon'];
+  listOfTypes.forEach((type) => {
+    const typeButton = screen.getByRole('button', { name: type });
+    expect(typeButton).toBeInTheDocument();
+
+    userEvent.click(typeButton);
+
+    const typeText = screen.getByTestId('pokemon-type', { name: type });
+    expect(typeText).toBeInTheDocument();
+  });
+});
+
+test('Verify integrity of "All" loop', () => {
+  renderWithRouter(<App />);
+
+  const allButton = screen.getByRole('button', { name: 'All' });
+  const nextButton = screen.getByRole('button', { name: 'Próximo Pokémon' });
 
   const pikachuGIF = screen.getByRole('img', { name: 'Pikachu sprite' });
   expect(pikachuGIF).toBeInTheDocument();
 
   userEvent.click(allButton);
-  userEvent.click(nextButton);
-
-  const charmanderGIF = screen.getByRole('img', { name: 'Charmander sprite' });
-  expect(charmanderGIF).toBeInTheDocument();
-
-  userEvent.click(nextButton);
-  userEvent.click(nextButton);
-  userEvent.click(nextButton);
-  userEvent.click(nextButton);
+  for (let i = 0; i < 5; i += 1) { userEvent.click(nextButton); }
 
   const mewGIF = screen.getByRole('img', { name: 'Mew sprite' });
   expect(mewGIF).toBeInTheDocument();
 
-  userEvent.click(nextButton);
-  userEvent.click(nextButton);
-  userEvent.click(nextButton);
-  userEvent.click(nextButton);
+  for (let i = 0; i < 4; i += 1) { userEvent.click(nextButton); }
 
   const newPikachuGIF = screen.getByRole('img', { name: 'Pikachu sprite' });
   expect(newPikachuGIF).toBeInTheDocument();
 
   const typeButtonCount = screen.getAllByTestId('pokemon-type-button');
   expect(typeButtonCount).toHaveLength(7);
-
-  userEvent.click(fireButton);
-  userEvent.click(nextButton);
-
-  const rapidashGIF = screen.getByRole('img', { name: 'Rapidash sprite' });
-  expect(rapidashGIF).toBeInTheDocument();
 });
